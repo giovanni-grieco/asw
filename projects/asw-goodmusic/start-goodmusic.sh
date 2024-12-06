2c997cd6629ee956558ab78717b612a89ccbc09b
giovanni <giovi.5057@gmail.com>
2024-12-06 14:52:27 +0100

    Spostata roba kafka nel branch "messaging"

--- projects/asw-goodmusic/start-goodmusic.sh
+++ projects/asw-goodmusic/start-goodmusic.sh
@@ -8,12 +8,6 @@ docker run -d --hostname localhost --name asw-consul --network host docker.io/ha
 docker run -d --hostname localhost --name recensioni-DB -p 5432:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=recensioni -d postgres
 docker run -d --hostname localhost --name connessioni-DB -p 5433:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=connessioni -d postgres
 docker run -d --hostname localhost --name recensioni-seguite-DB -p 5434:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=recensioni-seguite -d postgres
-docker compose up -d
-
-KAFKA_DOCKER=$(docker ps | grep kafka | grep -v zookeeper | awk '{print $1}')
-
-docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic recensioni --replication-factor 1 --partitions 4
-docker exec -it $KAFKA_DOCKER kafka-topics.sh --bootstrap-server localhost:9092 --create --topic connessioni --replication-factor 1 --partitions 4
 
 
 sleep 4
