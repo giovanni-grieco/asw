6605bdb75e652c56d77323e8b2e9b824fdfccb00
giovanni <giovi.5057@gmail.com>
2024-12-07 11:54:57 +0100

    Modificato script di avvio start-goodmusic.sh
    con -b o --build si fa "gradle build" prima di avviare il compose
    
    con -c o --clean si fa "gradle clean" prima di fare "gradle build"
    
    Esempio di utilizzo: ./start-goodmusic --clean --build
    
    Questo evita alcuni bug dovuti al gradle build che fa cache di alcuni file e classi.

--- projects/asw-goodmusic/start-goodmusic.sh
+++ projects/asw-goodmusic/start-goodmusic.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#If one of the parameters $1 or $2 is -c or --clean then
 
-gradle build
+if [[ "$1" == "-h" || "$1" == "--help" || "$2" == "-h" || "$2" == "--help" ]]; then
+    echo "Usage: $0 [-c|--clean] [-b|--build]"
+    echo "  -c, --clean  Clean the project"
+    echo "  -b, --build  Build the project"
+    exit 0
+fi
+
+if [[ "$1" == "-c" || "$1" == "--clean" || "$2" == "-c" || "$2" == "--clean" ]]; then
+    gradle clean
+fi
+
+if [[ "$1" == "-b" || "$1" == "--build" || "$2" == "-b" || "$2" == "--build"  ]]; then
+    gradle build
+fi
+
 docker compose up -d --build
